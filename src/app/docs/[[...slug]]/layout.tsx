871c60fbb315c7237a4bb63335c5c9ae2249090c
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-28 23:27:19 +0100

    fix broken segment

--- src/app/docs/[[...slug]]/layout.tsx
+++ src/app/docs/[[...slug]]/layout.tsx
@@ -10,13 +10,13 @@ export async function generateMetadata({ params }): Promise<Metadata> {
 
 export default function DocsLayout(props: {
   children: React.ReactNode;
-  params: { slug: string[] };
+  params: { slug?: string[] };
 }) {
   return (
     <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
       <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-r-zinc-100 py-10 dark:border-r-zinc-700 md:sticky md:block">
         <DocsSidebarNav
-          pathname={`/docs/${props.params.slug.join('/')}`}
+          pathname={`/docs/${props.params.slug?.join('/')}`}
           items={[
             { title: 'Introduction', href: '/docs/introduction' },
             { title: 'useQuery', href: '/docs/react/use-query' },
