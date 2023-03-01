97f3a995312494ae4cac2d5c5e210ff4f0fe9395
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-01 14:02:26 +0100

    fix broken sidebar links

--- src/app/features/[feature]/layout.tsx
+++ src/app/features/[feature]/layout.tsx
@@ -10,10 +10,10 @@ export default function FeaturesLayout(props: {
     <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
       <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-r-zinc-100 py-10 dark:border-r-zinc-700 md:sticky md:block">
         <DocsSidebarNav
-          pathname={`/${feature}`}
+          pathname={`/features/${feature}`}
           items={[
-            { title: 'Forms', href: '/forms' },
-            { title: 'Authentication', href: '/authentication' },
+            { title: 'Forms', href: '/features/forms' },
+            { title: 'Authentication', href: '/features/authentication' },
           ]}
         />
       </aside>
