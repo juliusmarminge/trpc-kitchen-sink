34ceac76d1a50143f2855c67b478a4181eae4e09
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-01 14:01:28 +0100

    singleton wc

--- src/utils/web-container.ts
+++ src/utils/web-container.ts
@@ -1,3 +1,9 @@
 import { WebContainer } from '@webcontainer/api';
 
-export const wc = WebContainer.boot();
+/**
+ * Workaround to make sure that the WebContainer is only booted once.
+ */
+const globalForWc = globalThis as unknown as { wc?: Promise<WebContainer> };
+
+export const wc = globalForWc.wc ?? WebContainer.boot();
+if (process.env.NODE_ENV !== 'production') globalForWc.wc = wc;
